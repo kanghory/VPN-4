287e5c6df55bd145320814dd4f3ab38817f4aab8
kanghory <132531297+kanghory@users.noreply.github.com>
2023-06-20 13:34:54 +0700

    Update up.sh

--- up.sh
+++ up.sh
@@ -15,7 +15,7 @@ rm -rf slow
 # download script
 cd /usr/bin
 # menu
-wget -O menu "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/menu.sh"
+wget -O menu "https://raw.githubusercontent.com/kanghory/VPN-4/main/menu/menu.sh"
 wget -O genssl "https://raw.githubusercontent.com/Andyyuda/ver3/main/ssh/genssl.sh"
 wget -O cf "https://raw.githubusercontent.com/Andyyuda/ver3/main/cf.sh"
 wget -O slow "https://raw.githubusercontent.com/Andyyuda/ver3/main/slow.sh"
@@ -25,7 +25,7 @@ wget -O trial "https://raw.githubusercontent.com/Andyyuda/ver3/main/ssh/trial.sh
 # chomad
 wget -q -O /usr/bin/up "https://raw.githubusercontent.com/Andyyuda/ver3/main/up.sh" && chmod +x /usr/bin/up
 wget -q -O /usr/bin/cf "https://raw.githubusercontent.com/Andyyuda/ver3/main/cf.sh" && chmod +x /usr/bin/cf
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/kanghory/VPN-4/main/menu/menu.sh" && chmod +x /usr/bin/menu
 
 chmod +x slow
 chmod +x menu-ssh
